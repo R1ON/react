af267c77935f70ee81f9e3d902e90326cc08ec32
Roman Maksimov <get.my.email.name@yandex.ru>
2022-12-04 13:58:20 +0600

    react5+6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { hello2 } from './myFolder';
+import { hello } from './myFolder/hello';
+
+// import fjioeqjfqe from './test';
+
+// import hello23, { TEST1, TEST2 } from './test';
+
+// console.log(hello23);
+
+import Dog from './dog.jpg';
+
 import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+console.log(hello2, hello);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
+    <div className='hello'>
+      <img src={Dog} />
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// import / export 
+
+// export const TEST = ...;
+// export { TEST, testFunction } 
+// import { TEST } from './......'
+
+
+// export default TEST;
+// import HJUFEHQFQJEOF from './......'
